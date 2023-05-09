19a2e84d18d85e9277adf986cf0044f5dcb22d50
José Valter Silva Carneiro <git@josevaltersilvacarneiro.net>
2023-05-09 13:21:00 -0300

    Adding the license

--- Settings/Settings.php
+++ Settings/Settings.php
@@ -1,5 +1,27 @@
 <?php
 
+/**
+ * This package stores the settings
+ * and sets the globals variables.
+ *
+ * Copyright (C) 2023, José V S Carneiro
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * @package	Settings
+ */
+
 date_default_timezone_set('America/Bahia');		// default timezone
 
 define('__VERSION__',	'v1');				// current code version
