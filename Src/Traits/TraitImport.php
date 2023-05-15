34e5af57d6fd6b5d8748abe0484c209515941f50
José Valter Silva Carneiro <git@josevaltersilvacarneiro.net>
2023-05-15 12:54:35 -0300

    Improving the doc

--- Src/Traits/TraitImport.php
+++ Src/Traits/TraitImport.php
@@ -19,7 +19,7 @@
  * You should have received a copy of the GNU General Public License 
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  * 
- * @package     Src\Traits
+ * @package     Josevaltersilvacarneiro\Html\Src\Traits
  */
 
 namespace Josevaltersilvacarneiro\Html\Src\Traits;
