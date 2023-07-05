f74cfb68b489cf52247f09ab984147905f6f4555
José Valter Silva Carneiro <git@josevaltersilvacarneiro.net>
2023-07-05 14:01:59 -0300

    Adding the package name

--- Src/Classes/Render/RenderInterface.php
+++ Src/Classes/Render/RenderInterface.php
@@ -19,7 +19,7 @@
  * You should have received a copy of the GNU General Public License 
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  * 
- * @package     Src\Classes\Render
+ * @package     Josevaltersilvacarneiro\Html\Src\Classes\Render
  */
 
 namespace Josevaltersilvacarneiro\Html\Src\Classes\Render;
