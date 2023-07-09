fd1883d01be9d4d1d41c3441c4484a51d8b665d1
José Valter Silva Carneiro <git@josevaltersilvacarneiro.net>
2023-07-09 13:54:53 -0300

    Adding the Service main class

---
+++ App/Model/Service/Service.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This package is responsible for providing functionalities
+ * and utilities to access the database and control the entities
+ * within the application. It serves as an intermediary layer
+ * between the application's business logic and the underlying
+ * database, offering services for data retrieval, manipulation,
+ * and entity management.
+ * 
+ * It offers services for managing relationships between entities
+ * in the database. It provides utilities for defining and enforcing
+ * relationships such as one-to-one, one-to-many, or many-to-many
+ * associations. It simplifies the handling of entity relationships
+ * and supports efficient data retrieval across related entities.
+ *
+ * Copyright (C) 2023, José V S Carneiro
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,    
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * @package     Josevaltersilvacarneiro\Html\App\Model\Service
+ */
+
+namespace Josevaltersilvacarneiro\Html\App\Model\Service;
+
+abstract class Service
+{}
