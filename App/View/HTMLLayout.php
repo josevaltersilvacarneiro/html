838814fb614616f293c9e20974164ee8aa0e1787
José Valter Silva Carneiro <git@josevaltersilvacarneiro.net>
2023-08-06 20:59:16 -0300

    Replacing the bootstrap files and adding the location of the page alert

--- App/View/HTMLLayout.php
+++ App/View/HTMLLayout.php
@@ -89,9 +89,11 @@ header('Content-Type: text/html;charset=UTF-8');
 		<?php $this->addHeader() ?>
 		<?php $this->addMain() ?>
 		<?php $this->addFooter() ?>
+		<div id="liveAlertPlaceholder"></div>
 	</body>
 	<!-- BEGIN bootstrap -->
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js" integrity="sha384-Rx+T1VzGupg4BHQYs2gCW9It+akI2MM/mndMCy36UVfodzcJcF0GGLxZIzObiEfa" crossorigin="anonymous"></script>
 	<!-- ENDOF bootstrap -->
 	<script type="module" src="<?=__JS__ . 'Public/app.js'?>"></script>
 </html>
