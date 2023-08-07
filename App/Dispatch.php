ad63870f3762c8c047ab21fba28c70f5d51e8a37
José Valter Silva Carneiro <git@josevaltersilvacarneiro.net>
2023-08-07 18:01:08 -0300

    Following psr12

--- App/Dispatch.php
+++ App/Dispatch.php
@@ -21,7 +21,7 @@ declare(strict_types=1,encoding="UTF-8");
  * You should have received a copy of the GNU General Public License 
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  * 
- * @package     Josevaltersilvacarneiro\Html\App
+ * @package Josevaltersilvacarneiro\Html\App
  */
 
 namespace Josevaltersilvacarneiro\Html\App;
@@ -35,162 +35,163 @@ use Josevaltersilvacarneiro\Html\Src\Classes\Log\RequestLog;
  * calls the method and passes the parameters
  * appropriately.
  *
- * @var Controller	$obj		the controller - for example, Home
- * @var string		$method		method that belongs to the $obj
- * @var string		$parameters	parameters of the $method
+ * @var Controller    $obj        the controller - for example, Home
+ * @var string        $method        method that belongs to the $obj
+ * @var string        $parameters    parameters of the $method
  *
- * @method void addController()	sets up the controller object
- * @method void addMethod()		sets up a method in $obj
- * @method void addParameters()	sets up the params in $method
+ * @method void addController()    sets up the controller object
+ * @method void addMethod()        sets up a method in $obj
+ * @method void addParameters()    sets up the params in $method
  *
- * @author		José V S Carneiro <git@josevaltersilvacarneiro.net>
- * @version		0.4
- * @see			Josevaltersilvacarneiro\Html\Src\Classes\Routes\Route
- * @copyright	Copyright (C) 2023, José V S Carneiro
- * @license		GPLv3
+ * @author    José V S Carneiro <git@josevaltersilvacarneiro.net>
+ * @version   0.4
+ * @see       Josevaltersilvacarneiro\Html\Src\Classes\Routes\Route
+ * @copyright Copyright (C) 2023, José V S Carneiro
+ * @license   GPLv3
  */
 
 final class Dispatch extends Route
 {
-	private Controller	$obj;
-	private ?string		$method;
-	private array		$parameters = array();
+    private Controller    $obj;
+    private ?string        $method;
+    private array        $parameters = array();
 
-	/**
-	 * Initializes the object.
-	 * 
-	 * @return		void
-	 * 
-	 * @author		José V S Carneiro <git@josevaltersilvacarneiro.net>
-	 * @version		0.2
-	 * @access		public
-	 * @see			https://www.php.net/manual/en/function.is-null.php
-	 * @see			https://www.php.net/manual/en/function.method-exists.php
-	 * @see			https://www.php.net/manual/en/function.call-user-func-array.php
-	 * @copyright	Copyright (C) 2023, José V S Carneiro
- 	 * @license		GPLv3
-	 */
+    /**
+     * Initializes the object.
+     * 
+     * @return void
+     * 
+     * @author    José V S Carneiro <git@josevaltersilvacarneiro.net>
+     * @version   0.2
+     * @access    public
+     * @see       https://www.php.net/manual/en/function.is-null.php
+     * @see       https://www.php.net/manual/en/function.method-exists.php
+     * @see       https://www.php.net/manual/en/function.call-user-func-array.php
+     * @copyright Copyright (C) 2023, José V S Carneiro
+     * @license   GPLv3
+     */
 
-	public function __construct()
-	{
-		$this->addController();
-		$this->addMethod();
-		$this->addParameters();
-		
-		try {
-			if (!is_null($this->getMethod()) &&
-				method_exists($this->obj, $this->getMethod())) {
+    public function __construct()
+    {
+        $this->addController();
+        $this->addMethod();
+        $this->addParameters();
+        
+        try {
+            if (!is_null($this->getMethod()) 
+                && method_exists($this->obj, $this->getMethod())
+            ) {
 
-				// if the method isn't null and exists
-				// in the controller, call it
-				
-				call_user_func_array(
-					[
-						$this->obj,
-						$this->getMethod()
-					],
-					$this->getParameters()
-				);
-			}
-		} catch (\BadMethodCallException $e) {
+                // if the method isn't null and exists
+                // in the controller, call it
+                
+                call_user_func_array(
+                    [
+                    $this->obj,
+                    $this->getMethod()
+                    ],
+                    $this->getParameters()
+                );
+            }
+        } catch (\BadMethodCallException $e) {
 
-			// if an exception was thrown here, then
-			// the application user made a terrible
-			// request. IT COULD BE A HACKER ATTACK
-			
-			$log = new RequestLog();
-			$log->setFile(__FILE__);
-			$log->setLine(__LINE__);
+            // if an exception was thrown here, then
+            // the application user made a terrible
+            // request. IT COULD BE A HACKER ATTACK
+            
+            $log = new RequestLog();
+            $log->setFile(__FILE__);
+            $log->setLine(__LINE__);
 
-			$user = $this->obj->getSession()->getSessionuser()->getUsername();
+            $user = $this->obj->getSession()->getSessionuser()->getUsername();
 
-			$log->store("The user ${user} made a bad request # " . $e->getMessage());
-		}
+            $log->store("The user ${user} made a bad request # " . $e->getMessage());
+        }
 
-		$this->obj->renderLayout();
-	}
+        $this->obj->renderLayout();
+    }
 
-	public function setMethod(?string $method): void
-	{
-		$this->method = $method;
-	}
+    public function setMethod(?string $method): void
+    {
+        $this->method = $method;
+    }
 
-	public function setParameters(array $parameters): void
-	{
-		$this->parameters = $parameters;
-	}
+    public function setParameters(array $parameters): void
+    {
+        $this->parameters = $parameters;
+    }
 
-	public function getMethod(): ?string
-	{
-		return $this->method;
-	}
+    public function getMethod(): ?string
+    {
+        return $this->method;
+    }
 
-	public function getParameters(): array
-	{
-		return $this->parameters;
-	}
+    public function getParameters(): array
+    {
+        return $this->parameters;
+    }
 
-	/**
-	 * This method instantiates the CONTROLLER dynamically
-	 * according to the service that the user requested.
-	 * The route property returns this service.
-	 * 
-	 * @return		void
-	 * 
-	 * @author		José V S Carneiro <git@josevaltersilvacarneiro.net>
-	 * @version		0.1
-	 * @access		private
-	 * @see			https://www.php.net/manual/en/language.oop5.basic.php#language.oop5.basic.new
-	 * @copyright	Copyright (C) 2023, José V S Carneiro
- 	 * @license		GPLv3
-	 */
+    /**
+     * This method instantiates the CONTROLLER dynamically
+     * according to the service that the user requested.
+     * The route property returns this service.
+     * 
+     * @return void
+     * 
+     * @author    José V S Carneiro <git@josevaltersilvacarneiro.net>
+     * @version   0.1
+     * @access    private
+     * @see       https://www.php.net/manual/en/language.oop5.basic.php#language.oop5.basic.new
+     * @copyright Copyright (C) 2023, José V S Carneiro
+     * @license   GPLv3
+     */
 
-	private function addController(): void
-	{
-		$className = 'Josevaltersilvacarneiro\\Html\\App\\Controller\\' .
-			$this->route . '\\' . $this->route;
-		
-		$this->obj = new $className;
-	}
+    private function addController(): void
+    {
+        $className = 'Josevaltersilvacarneiro\\Html\\App\\Controller\\' .
+        $this->route . '\\' . $this->route;
+        
+        $this->obj = new $className;
+    }
 
-	/**
-	 * This method sets the second parameter
-	 * passed in url as method in $this->obj.
-	 * 
-	 * @return		void
-	 * 
-	 * @author		José V S Carneiro <git@josevaltersilvacarneiro.net>
-	 * @version		0.2
-	 * @access		private
-	 * @see			https://www.php.net/manual/en/migration70.new-features.php#migration70.new-features.null-coalesce-op
-	 * @copyright	Copyright (C) 2023, José V S Carneiro
- 	 * @license		GPLv3
-	 */
+    /**
+     * This method sets the second parameter
+     * passed in url as method in $this->obj.
+     * 
+     * @return void
+     * 
+     * @author    José V S Carneiro <git@josevaltersilvacarneiro.net>
+     * @version   0.2
+     * @access    private
+     * @see       https://www.php.net/manual/en/migration70.new-features.php#migration70.new-features.null-coalesce-op
+     * @copyright Copyright (C) 2023, José V S Carneiro
+     * @license   GPLv3
+     */
 
-	private function addMethod(): void
-	{
-		$methodName = $this->url[1] ?? null;
-		
-		$this->setMethod(method: $methodName);
-	}
+    private function addMethod(): void
+    {
+        $methodName = $this->url[1] ?? null;
+        
+        $this->setMethod(method: $methodName);
+    }
 
-	/**
-	 * This method sets the parameters passed
-	 * by the user in url.
-	 * 
-	 * @return		void
-	 * 
-	 * @author		José V S Carneiro <git@josevaltersilvacarneiro.net>
-	 * @version		0.2
-	 * @access		private
-	 * @see			https://www.php.net/manual/en/function.array-slice.php
-	 * @copyright	Copyright (C) 2023, José V S Carneiro
- 	 * @license		GPLv3
-	 */
+    /**
+     * This method sets the parameters passed
+     * by the user in url.
+     * 
+     * @return void
+     * 
+     * @author    José V S Carneiro <git@josevaltersilvacarneiro.net>
+     * @version   0.2
+     * @access    private
+     * @see       https://www.php.net/manual/en/function.array-slice.php
+     * @copyright Copyright (C) 2023, José V S Carneiro
+     * @license   GPLv3
+     */
 
-	private function addParameters(): void
-	{
-		$parameters = array_slice($this->url, 2);
-		$this->setParameters($parameters);
-	}
+    private function addParameters(): void
+    {
+        $parameters = array_slice($this->url, 2);
+        $this->setParameters($parameters);
+    }
 }
