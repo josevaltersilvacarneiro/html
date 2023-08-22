285ff4e27f2ffdf60b2e4ec20f6a24bce90cac36
José Valter Silva Carneiro <git@josevaltersilvacarneiro.net>
2023-08-22 20:37:36 -0300

    Adding the NameAttributeInterface

---
+++ Src/Interfaces/Attributes/NameAttributeInterface.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Based on the project requirements, the interfaces will be written.
+ * PHP VERSION >= 8.2.0
+ * 
+ * Copyright (C) 2023, José V S Carneiro
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by  
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,    
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * @category Attributes
+ * @package  Josevaltersilvacarneiro\Html\Src\Interfaces\Attributes
+ * @author   José Carneiro <git@josevaltersilvacarneiro.net>
+ * @license  GPLv3 https://www.gnu.org/licenses/quick-guide-gplv3.html
+ * @link     https://github.com/josevaltersilvacarneiro/html/tree/main/Src/Interfaces/Attributes
+ */
+
+namespace Josevaltersilvacarneiro\Html\Src\Interfaces\Attributes;
+
+use Josevaltersilvacarneiro\Html\Src\Interfaces\Attributes\AttributeInterface;
+
+/**
+ * This interface represents a fullname.
+ * 
+ * @category  NameAttributeInterface
+ * @package   Josevaltersilvacarneiro\Html\Src\Interfaces\Attributes
+ * @author    José Carneiro <git@josevaltersilvacarneiro.net>
+ * @copyright 2023 José Carneiro
+ * @license   GPLv3 https://www.gnu.org/licenses/quick-guide-gplv3.html
+ * @version   Release: 0.0.1
+ * @link      https://github.com/josevaltersilvacarneiro/html/tree/main/Src/Interfaces/Attributes
+ */
+interface NameAttributeInterface extends AttributeInterface
+{
+    /**
+     * This method sets the complete name.
+     * 
+     * @param string $completeName The complete name @example José Carneiro
+     * 
+     * @return static
+     */
+    public function setCompleteName(string $completeName): static;
+
+    /**
+     * This method returns the first name.
+     * 
+     * @return string The first name @example José
+     */
+    public function getFirstName(): string;
+
+    /**
+     * This method returns the last name.
+     * 
+     * @return string The last name @example Carneiro
+     */
+    public function getLastName(): string;
+}
