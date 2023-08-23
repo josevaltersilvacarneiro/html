efc3d7a71d616767ee83111b7f1a309e919f446e
José Valter Silva Carneiro <git@josevaltersilvacarneiro.net>
2023-08-23 16:02:09 -0300

    Adding ControllerExceptionInterface

---
+++ Src/Interfaces/Exceptions/ControllerExceptionInterface.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Based on the project requirements, the interfaces will be written.
+ * PHP VERSION >= 8.2.0
+ * 
+ * Copyright (C) 2023, José Carneiro
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by  
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,    
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * @category Exceptions
+ * @package  Josevaltersilvacarneiro\Html\Src\Interfaces\Exceptions
+ * @author   José Carneiro <git@josevaltersilvacarneiro.net>
+ * @license  GPLv3 https://www.gnu.org/licenses/quick-guide-gplv3.html
+ * @link     https://github.com/josevaltersilvacarneiro/html/tree/main/Src/Interfaces/Exceptions
+ */
+
+namespace Josevaltersilvacarneiro\Html\Src\Interfaces\Exceptions;
+
+use Josevaltersilvacarneiro\Html\Src\Interfaces\Exceptions\HtmlExceptionInterface;
+
+/**
+ * This exception is thrown when occurs an error in attributes operations.
+ * 
+ * @category  ControllerExceptionInterface
+ * @package   Josevaltersilvacarneiro\Html\Src\Interfaces\Exceptions
+ * @author    José Carneiro <git@josevaltersilvacarneiro.net>
+ * @copyright 2023 José Carneiro
+ * @license   GPLv3 https://www.gnu.org/licenses/quick-guide-gplv3.html
+ * @version   Release: 0.0.1
+ * @link      https://github.com/josevaltersilvacarneiro/html/tree/main/Src/Interfaces/Exceptions
+ */
+interface ControllerExceptionInterface extends HtmlExceptionInterface
+{
+    
+}
