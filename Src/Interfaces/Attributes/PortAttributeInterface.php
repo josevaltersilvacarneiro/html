e631ba7572f33eea2a5ba50ece1a082a888ea849
José Valter Silva Carneiro <git@josevaltersilvacarneiro.net>
2023-08-22 21:07:29 -0300

    Adding the port attribute

---
+++ Src/Interfaces/Attributes/PortAttributeInterface.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Based on the project requirements, the interfaces will be written.
+ * PHP VERSION >= 8.2.0
+ * 
+ * Copyright (C) 2023, José V S Carneiro
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by  
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,    
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * @category Attributes
+ * @package  Josevaltersilvacarneiro\Html\Src\Interfaces\Attributes
+ * @author   José Carneiro <git@josevaltersilvacarneiro.net>
+ * @license  GPLv3 https://www.gnu.org/licenses/quick-guide-gplv3.html
+ * @link     https://github.com/josevaltersilvacarneiro/html/tree/main/Src/Interfaces/Attributes
+ */
+
+namespace Josevaltersilvacarneiro\Html\Src\Interfaces\Attributes;
+
+use Josevaltersilvacarneiro\Html\Src\Interfaces\Attributes\AttributeInterface;
+
+/**
+ * This interface represents an access port.
+ * 
+ * Warning: To access an application using HTTP, the user must
+ * to make a connection using the TCP protocol that addresses
+ * to an IP address and a port number. However, to receive the
+ * request, he must inform his port number and his IP address.
+ * Although he might cheat, the ISP would block the connection.
+ * Therefore, this information should be stored for security
+ * audit purposes and threat detection.
+ * 
+ * @category  PortAttributeInterface
+ * @package   Josevaltersilvacarneiro\Html\Src\Interfaces\Attributes
+ * @author    José Carneiro <git@josevaltersilvacarneiro.net>
+ * @copyright 2023 José Carneiro
+ * @license   GPLv3 https://www.gnu.org/licenses/quick-guide-gplv3.html
+ * @version   Release: 0.0.1
+ * @link      https://github.com/josevaltersilvacarneiro/html/tree/main/Src/Interfaces/Attributes
+ */
+interface PortAttributeInterface extends AttributeInterface
+{
+    
+}
