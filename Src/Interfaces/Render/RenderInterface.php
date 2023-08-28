10dff6922caef946ffb5fd970d23f03caee2739a
José Carneiro <git@josevaltersilvacarneiro.net>
2023-08-28 21:37:05 +0000

    Adding the RenderInterface

---
+++ Src/Interfaces/Render/RenderInterface.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Based on the project requirements, the interfaces will be written.
+ * PHP VERSION >= 8.2.0
+ * 
+ * Copyright (C) 2023, José V S Carneiro
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by  
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,    
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * @category Render
+ * @package  Josevaltersilvacarneiro\Html\Src\Interfaces\Render
+ * @author   José Carneiro <git@josevaltersilvacarneiro.net>
+ * @license  GPLv3 https://www.gnu.org/licenses/quick-guide-gplv3.html
+ * @link     https://github.com/josevaltersilvacarneiro/html/tree/main/Src/Interfaces/Render
+ */
+
+namespace Josevaltersilvacarneiro\Html\Src\Interfaces\Render;
+
+/**
+ * This interface represents the render of the page.
+ * 
+ * @category  RenderInterface
+ * @package   Josevaltersilvacarneiro\Html\Src\Interfaces\Render
+ * @author    José Carneiro <git@josevaltersilvacarneiro.net>
+ * @copyright 2023 José Carneiro
+ * @license   GPLv3 https://www.gnu.org/licenses/quick-guide-gplv3.html
+ * @version   Release: 0.0.1
+ * @link      https://github.com/josevaltersilvacarneiro/html/tree/main/Src/Interfaces/Render
+ */
+interface RenderInterface
+{
+    /**
+     * This method reders the layout and returns it.
+     * 
+     * @return string What is rendered for the user
+     */
+    public function renderLayout(): string;
+}
