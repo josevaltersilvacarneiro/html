00858406dc38ed30ac2c521acf713ab21c5128eb
José Carneiro <git@josevaltersilvacarneiro.net>
2023-08-28 23:17:40 +0000

    Improving the comments

--- Src/Classes/Dao/GenericDao.php
+++ Src/Classes/Dao/GenericDao.php
@@ -10,17 +10,17 @@ declare(strict_types=1);
  * PHP VERSION >= 8.2.0
  * 
  * Copyright (C) 2023, José V S Carneiro
- *
+ * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful,    
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public License 
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  * 
@@ -52,7 +52,7 @@ use Josevaltersilvacarneiro\Html\Src\Classes\Sql\SanitizeSql;
  * @author    José Carneiro <git@josevaltersilvacarneiro.net>
  * @copyright 2023 José Carneiro
  * @license   GPLv3 https://www.gnu.org/licenses/quick-guide-gplv3.html
- * @version   Release: 0.3.0
+ * @version   Release: 0.3.1
  * @link      https://github.com/josevaltersilvacarneiro/html/tree/main/Src/Interfaces/Dao
  * @see       https://www.php.net/manual/en/function.list
  */
