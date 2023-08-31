7e0f10cca5abfd07ef756ffce2f4ce2baa30861e
José Carneiro <git@josevaltersilvacarneiro.net>
2023-08-31 23:57:13 +0000

    Adding the ConnectInterface

---
+++ Src/Interfaces/Sql/ConnectInterface.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Based on the project requirements, the interfaces will be written.
+ * PHP VERSION >= 8.2.0
+ * 
+ * Copyright (C) 2023, José V S Carneiro
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by  
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,    
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * @category Sql
+ * @package  Josevaltersilvacarneiro\Html\Src\Interfaces\Sql
+ * @author   José Carneiro <git@josevaltersilvacarneiro.net>
+ * @license  GPLv3 https://www.gnu.org/licenses/quick-guide-gplv3.html
+ * @link     https://github.com/josevaltersilvacarneiro/html/tree/main/Src/Interfaces/Sql
+ */
+
+namespace Josevaltersilvacarneiro\Html\Src\Interfaces\Sql;
+
+use Josevaltersilvacarneiro\Html\Src\Interfaces\Exceptions\ConnectExceptionInterface;
+
+/**
+ * This interface represents the Connection interface.
+ * 
+ * @category  ConnectInterface
+ * @package   Josevaltersilvacarneiro\Html\Src\Interfaces\Sql
+ * @author    José Carneiro <git@josevaltersilvacarneiro.net>
+ * @copyright 2023 José Carneiro
+ * @license   GPLv3 https://www.gnu.org/licenses/quick-guide-gplv3.html
+ * @version   Release: 0.0.1
+ * @link      https://github.com/josevaltersilvacarneiro/html/tree/main/Src/Interfaces/Sql
+ */
+interface ConnectInterface
+{
+    /**
+     * Initializes the connection.
+     * 
+     * @return \PDO The connection
+     * @throws ConnectExceptionInterface If the connection fails.
+     */
+    public static function newMysqlConnection(): \PDO;
+}
