e263064b1e7365bca74e3f127cce6d5656a36a81
José Carneiro <git@josevaltersilvacarneiro.net>
2023-09-10 00:17:16 +0000

    Improving the algorithm

--- App/Model/Attributes/HashAttribute.php
+++ App/Model/Attributes/HashAttribute.php
@@ -42,12 +42,12 @@ use Josevaltersilvacarneiro\Html\Src\Classes\Exceptions\AttributeException;
  * @author    José Carneiro <git@josevaltersilvacarneiro.net>
  * @copyright 2023 José Carneiro
  * @license   GPLv3 https://www.gnu.org/licenses/quick-guide-gplv3.html
- * @version   Release: 0.0.1
+ * @version   Release: 0.0.2
  * @link      https://github.com/josevaltersilvacarneiro/html/tree/main/App/Model/Attributes
  */
 final class HashAttribute implements HashAttributeInterface
 {
-    private const HASH_ALGO = PASSWORD_DEFAULT;
+    private const HASH_ALGO = PASSWORD_ARGON2ID;
 
     /**
      * Initializes the attribute.
