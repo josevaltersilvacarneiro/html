d1b9646f8a4c6efda8ae0b068a376c75354caedb
José Carneiro <git@josevaltersilvacarneiro.net>
2023-09-10 00:18:59 +0000

    Improving the _isHashValid method to avoid mistakes

--- App/Model/Attributes/HashAttribute.php
+++ App/Model/Attributes/HashAttribute.php
@@ -42,7 +42,7 @@ use Josevaltersilvacarneiro\Html\Src\Classes\Exceptions\AttributeException;
  * @author    José Carneiro <git@josevaltersilvacarneiro.net>
  * @copyright 2023 José Carneiro
  * @license   GPLv3 https://www.gnu.org/licenses/quick-guide-gplv3.html
- * @version   Release: 0.0.2
+ * @version   Release: 0.0.3
  * @link      https://github.com/josevaltersilvacarneiro/html/tree/main/App/Model/Attributes
  */
 final class HashAttribute implements HashAttributeInterface
@@ -161,6 +161,6 @@ final class HashAttribute implements HashAttributeInterface
     {
         $hashInfo = password_get_info($hash);
 
-        return $hashInfo['algoName'] !== 'unknown';
+        return $hashInfo['algoName'] === self::HASH_ALGO;
     }
 }
