459debd36628036f4e6d97656f469b2631872a10
José Carneiro <git@josevaltersilvacarneiro.net>
2023-09-11 23:55:11 +0000

    Adding the PrimaryKeyAttribute that is generated automatically, that's auto increment

---
+++ App/Model/Attributes/PrimaryKeyAttribute.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This package conatins the attributes of the entities.
+ * PHP VERSION >= 8.2.0
+ * 
+ * Copyright (C) 2023, José Carneiro
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by  
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,    
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * @category Attributes
+ * @package  Josevaltersilvacarneiro\Html\App\Model\Attributes
+ * @author   José Carneiro <git@josevaltersilvacarneiro.net>
+ * @license  GPLv3 https://www.gnu.org/licenses/quick-guide-gplv3.html
+ * @link     https://github.com/josevaltersilvacarneiro/html/tree/main/App/Model/Attributes
+ */
+
+namespace Josevaltersilvacarneiro\Html\App\Model\Attributes;
+
+use Josevaltersilvacarneiro\Html\Src\Interfaces\Attributes\
+    PrimaryKeyAttributeInterface;
+
+/**
+ * This class represents a Primary Key that is
+ * generated automatically.
+ * 
+ * @category  PrimaryKeyAttribute
+ * @package   Josevaltersilvacarneiro\Html\App\Model\Attributes
+ * @author    José Carneiro <git@josevaltersilvacarneiro.net>
+ * @copyright 2023 José Carneiro
+ * @license   GPLv3 https://www.gnu.org/licenses/quick-guide-gplv3.html
+ * @version   Release: 0.0.1
+ * @link      https://github.com/josevaltersilvacarneiro/html/tree/main/App/Model/Attributes
+ */
+class PrimaryKeyAttribute implements PrimaryKeyAttributeInterface
+{
+    private int $_id;
+
+    /**
+     * Returns the representation of this attribute.
+     * 
+     * @return mixed Id
+     */
+    public function getRepresentation(): mixed
+    {
+        return $this->_id;
+    }
+
+    /**
+     * Returns a new instance of this attribute.
+     * 
+     * @param mixed $value Id
+     * 
+     * @return static|null Itself on success; otherwise, null
+     */
+    public static function newInstance(mixed $value): ?static
+    {
+        $myself = new self();
+        $myself->_id = $value;
+        return $myself;
+    }
+}
