09eaae2c9d80a02b0178adbce1410f71109ae530
José Carneiro <git@josevaltersilvacarneiro.net>
2023-09-18 20:25:52 +0000

    Adding the Home Controller

---
+++ App/Controller/Home/Home.php
@@ -0,0 +1,97 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This is a comprehensive PHP package designed to streamline the development
+ * of controllers in the application following the MVC (Model-View-Controller)
+ * architectural pattern. It provides a set of powerful tools and utilities to
+ * handle user input, orchestrate application logic, and facilitate seamless
+ * communication between the Model and View components.
+ * PHP VERSION >= 8.2.0
+ * 
+ * Copyright (C) 2023, José V S Carneiro
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by  
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,    
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * @category Controllers
+ * @package  Josevaltersilvacarneiro\Html\App\Controllers
+ * @author   José Carneiro <git@josevaltersilvacarneiro.net>
+ * @license  GPLv3 https://www.gnu.org/licenses/quick-guide-gplv3.html
+ * @link     https://github.com/josevaltersilvacarneiro/html/tree/main/App/Controllers
+ */
+
+namespace Josevaltersilvacarneiro\Html\App\Controller\Home;
+
+use Josevaltersilvacarneiro\Html\App\Controller\HTMLController;
+use Josevaltersilvacarneiro\Html\Src\Interfaces\Entities\
+    SessionEntityInterface;
+
+use Nyholm\Psr7\Response;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+/**
+ * This class serves as the main controller for the system's home page,
+ * which acts as the primary entry point and central hub for the
+ * application.
+ * 
+ * @category  Home
+ * @package   Josevaltersilvacarneiro\Html\App\Controllers\Home
+ * @author    José Carneiro <git@josevaltersilvacarneiro.net>
+ * @copyright 2023 José Carneiro
+ * @license   GPLv3 https://www.gnu.org/licenses/quick-guide-gplv3.html
+ * @version   Release: 0.0.1
+ * @link      https://github.com/josevaltersilvacarneiro/html/tree/main/App/Cotrollers
+ */
+final class Home extends HTMLController
+{
+    /**
+     * Initializes the home page controller.
+     * 
+     * @param SessionEntityInterface $session session
+     */
+    public function __construct(private readonly SessionEntityInterface $session)
+    {
+        $this->setDir('Home');
+        $this->setTitle('HTML - Welcome');
+        $this->setDescription(
+            'The home page serves as the central hub of our system, providing users
+			with a comprehensive overview and access to various features. It welcomes
+			users with a personalized message and offers an intuitive navigation
+			menu to explore different sections of the system.'
+        );
+        $this->setKeywords('MVC SOLID josevaltersilvacarneiro Home');
+    }
+
+    /**
+     * Handles the request and returns a response.
+     * 
+     * @param ServerRequestInterface $request request
+     * 
+     * @return ResponseInterface response
+     */
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        if (!$this->session->isUserLogged()) {
+            return new Response(302, ['Location' => '/login']);
+        }
+
+        return new Response(
+            200, [
+            'Content-Type'    => 'text/html;charset=UTF-8'
+            ], parent::renderLayout()
+        );
+    }
+}
