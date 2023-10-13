ae87ac79d693f37c8381194671d52a1c0ac9bb6e
José V S Carneiro <git@josevaltersilvacarneiro.net>
2023-10-13 16:37:53 +0000

    Adding the unit testing to IpAttribute

---
+++ Tests/App/Model/Attributes/IpAttributeTest.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This package contains the unit testing of entity attributes.
+ * PHP VERSION >= 8.2.0
+ * 
+ * Copyright (C) 2023, José Carneiro
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by  
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,    
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * @category Attributes
+ * @package  Josevaltersilvacarneiro\Html\Tests\App\Model\Attributes
+ * @author   José Carneiro <git@josevaltersilvacarneiro.net>
+ * @license  GPLv3 https://www.gnu.org/licenses/quick-guide-gplv3.html
+ * @link     https://github.com/josevaltersilvacarneiro/html/tree/main/Tests/App/Model/Attributes
+ */
+
+namespace Josevaltersilvacarneiro\Html\Tests\App\Model\Attributes;
+
+use Josevaltersilvacarneiro\Html\App\Model\Attributes\IpAttribute;
+use Josevaltersilvacarneiro\Html\Src\Classes\Exceptions\AttributeException;
+
+use PHPUnit\Framework\TestCase;
+
+class IpAttributeTest extends TestCase
+{
+    public function testInitialization(): void
+    {
+        $ip1 = new IpAttribute('::1');
+        $this->assertInstanceOf(IpAttribute::class, $ip1);
+        $ip2 = new IpAttribute('127.0.0.1');
+        $this->assertInstanceOf(IpAttribute::class, $ip2);
+    }
+
+    public function testInitializationError(): void
+    {
+        $this->expectException(AttributeException::class);
+        new IpAttribute('192.168.0.256');
+    }
+}
